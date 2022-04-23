d7b34c73f987beea3e7dda0ab2839b7bd6946bf4
razbensimon <raz.bensimon@env0.com>
2022-04-23 18:59:33 +0300

    add unit tests jest framework

---
+++ src/__tests__/costs.test.js
@@ -0,0 +1,12 @@
+const request = require('supertest');
+const { app } = require('../server');
+
+describe('Post Endpoints', () => {
+  it('should create a new post', async () => {
+    const res = await request(app).post('/api/costs').send({
+      userId: 1,
+      title: 'test is cool'
+    });
+    expect(res.statusCode).toEqual(201);
+  });
+});
--- src/server.js
+++ src/server.js
@@ -2,10 +2,27 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.post('/api/costs', (req, res) => {
+  res.status(201).send('Hello World!');
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+class UserModel {
+  id;
+  first_name;
+  last_name;
+  birthday;
+  marital_status;
+}
+
+class CostItemModel {
+  id;
+  sum;
+  category;
+  description;
+}
+
+module.exports = { app };
